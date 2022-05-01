0d076e8f0fa4bad2ca7e0c6f14eae410fba53abc
Tom Kimpson <tomkimpson@gmail.com>
2022-05-01 15:17:01 +0000

    pd to TRF

---
+++ reproducible_workflow/scripts/dev/create_TRF_records.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas_tfrecords import pd2tf, tf2pd
+
+df = pd.DataFrame({'A': [1, 2, 3], 'B': ['a', 'b', 'c'], 'C': [[1, 2], [3, 4], [5, 6]]})
+
+print (df)
+
+# local
+print ('df to trf:')
+pd2tf(df, './tfrecords')
+
+print('load trf:')
+my_df = tf2pd('./tfrecords')
+print(my_df)
