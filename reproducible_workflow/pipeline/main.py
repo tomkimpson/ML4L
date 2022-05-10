875078bd644d4ea82a2686e4702e3c1b1378fc77
T <t1@Ts-MacBook-Pro.fritz.box>
2022-05-10 19:12:32 +0100

    structure of joining method

--- reproducible_workflow/pipeline/main.py
+++ reproducible_workflow/pipeline/main.py
@@ -4,20 +4,6 @@ from raw_data_processor.raw_data_processor import ProcessERAData
 from raw_data_processor.join_ERA_with_MODIS import JoinERAWithMODIS
 import argparse
  
-parser = argparse.ArgumentParser(description="Just an example",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-a", "--archive", action="store_true", help="archive mode")
-parser.add_argument("-v", "--verbose", action="store_true", help="increase verbosity")
-parser.add_argument("-B", "--block-size", help="checksum blocksize")
-parser.add_argument("--ignore-existing", action="store_true", help="skip files that exist")
-parser.add_argument("--exclude", help="files to exclude")
-parser.add_argument("src", help="Source location")
-parser.add_argument("dest", help="Destination location")
-args = parser.parse_args()
-config = vars(args)
-print(config)
-
-
 
 def process_raw_data():
 
