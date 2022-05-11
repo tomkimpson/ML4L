10baf8683ec0ee4a307e1cc140d9ea07fb0de8f7
T <t1@Ts-MacBook-Pro.local>
2022-05-11 13:58:59 +0100

    build in NN model to pipeline

--- reproducible_workflow/pipeline/model/neural_net.py
+++ reproducible_workflow/pipeline/model/neural_net.py
@@ -8,7 +8,7 @@ from dataloader.dataloader import DataLoader
 import tensorflow as tf
 import numpy as np
 import os
-
+import json
 
 class NeuralNet(BaseModel):
 
@@ -117,15 +117,12 @@ class NeuralNet(BaseModel):
         print('Training network with:')
         self._model_status()
 
-        history = self.model.fit(self.training_data[self.training_features], 
+        self.history = self.model.fit(self.training_data[self.training_features], 
                                  self.training_data[self.target_variable], 
                                  epochs=self.epochs, batch_size=self.batch_size,
                                  verbose=1,
                                  validation_data=(self.validation_data[self.training_features], self.validation_data[self.target_variable]),
                                  ) 
-        return history
-
-
 
     def _model_status(self):
 
@@ -139,40 +136,16 @@ class NeuralNet(BaseModel):
     def save_model(self):
 
         """Save model to disk after training """
-        
+        save_dir = self.path_to_trained_models + self.model_name
+
         #Create a directory where we will save everything
-        os.mkdir(self.path_to_trained_models + self.model_name)
+        os.mkdir(save_dir)
 
-    #  "train": {
-    #     "batch_size": 10000,
-    #     "buffer_size": 1000,
-    #     "epochs": 2,
-    #     "number_of_hidden_layers":2,
-    #     "nodes_per_layer": [None,None],
-    #     "val_subsplits": 5,
-    #     "training_features": ['sp', 'msl', 'u10', 'v10'],
-    #     "target_variable": 'MODIS_LST',
-    #     "learning_rate": 3e-4,
-    #     "loss": 'mse',
-    #     "metrics": ["accuracy"],
-    #     "path_to_trained_models": f'{root}processed_data/trained_models/',
-        
-        # #Create a directory
-        # os.makedirs(fout)
-        # print ('Writing data to:', fout)
 
-        
-        # #Save the trained NN and the training history
-        # model.save(fout+'trained_model') 
-        # history_dict = history.history
-        # json.dump(history_dict, open(fout+'history.json', 'w'))
-    
-        
-        # #Save meta data as a txt file
-        # with open(fout+'meta.txt', 'w') as f:
-        #     for k,v in parameters_dict.items():
-        #         row = k + ": " + str(v) + "\n"
-        #         f.write(row)
+        # Save the trained NN and the training history
+        self.model.save(save_dir+'trained_model') 
+        history_dict = self.history.history
+        json.dump(history_dict, open(save_dir+'history.json', 'w'))
+        json.dump(self.config, open(save_dir+'cfg.json', 'w'))
 
-        # return fout
 
