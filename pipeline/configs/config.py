aaa4cb641573e0f1a41bebc312c3e9bc5b669bba
Tom Kimpson <tomkimpson@gmail.com>
2022-05-20 13:56:01 +0100

    rerun V15

--- pipeline/configs/config.py
+++ pipeline/configs/config.py
@@ -104,7 +104,7 @@ CFG = {
         "metrics": ["accuracy"],
         "path_to_trained_models": f'{root}processed_data/trained_models/',
         "model_name": 'V15_2016_augmented',
-        "overwrite": False,
+        "overwrite": True,
         "use_pretrained_model":False,
         "epoch_save_freq": 10,
         "early_stopping_patience":20,
