001b08b56df1eb1fc2d25e1e9327fefa405750a1
T <t1@Ts-MacBook-Pro.fritz.box>
2022-05-27 11:42:56 +0100

    Trial RAPIDS nearest neighbours

--- pipeline/raw_data_processor/join_ERA_with_MODIS.py
+++ pipeline/raw_data_processor/join_ERA_with_MODIS.py
@@ -290,7 +290,7 @@ class JoinERAWithMODIS():
 
         Xq = np.deg2rad(np.c_[query_lats, query_lons])
         print ('Xq shape before cutoff', Xq.shape)
-        Xq = Xq[0:10]
+        Xq = Xq[0:30000]
 
         X_cudf = cudf.DataFrame(Xq)
 
