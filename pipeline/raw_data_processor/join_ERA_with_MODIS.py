7b973efb8c079cfd95a0ed4453fccabe098c181e
T <t1@Ts-MacBook-Pro.fritz.box>
2022-05-30 12:11:00 +0100

    RAPIDs explore brute

--- pipeline/raw_data_processor/join_ERA_with_MODIS.py
+++ pipeline/raw_data_processor/join_ERA_with_MODIS.py
@@ -319,12 +319,14 @@ class JoinERAWithMODIS():
 
         #Group it. Each ERA point has a bunch of MODIS points. Group and average
         df_grouped = df_filtered.groupby(['latitude_ERA','longitude_ERA']).mean()
-        df_grouped['counts'] = df_filtered.value_counts(subset=['latitude_ERA','longitude_ERA']) # Must be a way to combine this with the above line. Can used grouped agg, but then need to specify operation for each column?
+        df_grouped['number_of_modis_observations'] = df_filtered.value_counts(subset=['latitude_ERA','longitude_ERA']) # Must be a way to combine this with the above line. Can used grouped agg, but then need to specify operation for each column?
+        df_grouped = df_grouped.reset_index()    
+
 
         print ('This is the grouped output df')
-        print(df_grouped.columns)
+        #Drop any columns we dont care about
+        df_grouped = df_grouped.drop(['index_MODIS', 'band','spatial_ref','index_ERA','values','number','surface','depthBelowLandLayer'], axis=1) #get rid of all these columns that we dont need
         print(df_grouped)
-
 
         sys.exit()
 
