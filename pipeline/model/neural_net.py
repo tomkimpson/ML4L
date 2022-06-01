2dba914a8b5ba5f35199d5fad0c9656cd1b3b9b2
T <t1@Ts-MacBook-Pro.fritz.box>
2022-06-01 13:29:52 +0100

    test sequential mem allocation

--- pipeline/model/neural_net.py
+++ pipeline/model/neural_net.py
@@ -21,8 +21,8 @@ import time
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-
+   # tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)]
 class NeuralNet():
 
 
