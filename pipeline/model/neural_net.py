f40355b54307bfe33d95ee404ff375e2e4b957bd
T <t1@Ts-MacBook-Pro.fritz.box>
2022-06-01 12:41:24 +0100

    test sequential mem allocation

--- pipeline/model/neural_net.py
+++ pipeline/model/neural_net.py
@@ -18,6 +18,10 @@ from collections import Counter
 import gc
 import sys
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
 class NeuralNet():
 
     def __init__(self,cfg): 
@@ -161,18 +165,6 @@ class NeuralNet():
            Drops permuted_feature from the list of training features to
            allow for drop column feature importance tests"""
 
-
-
-
-
-
-
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-
-        print('gpus:', gpus)
-        print(dir(tf.config.experimental))
 
         #Drop this feature if it exists
         if permuted_feature is not None:
