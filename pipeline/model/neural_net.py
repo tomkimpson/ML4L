a9270a178849aa493169a73cc012f91e0487f1b9
T <t1@Ts-MacBook-Pro.fritz.box>
2022-06-01 12:33:59 +0100

    test sequential mem allocation

--- pipeline/model/neural_net.py
+++ pipeline/model/neural_net.py
@@ -283,7 +283,7 @@ class NeuralNet():
             tf.config.experimental.set_memory_growth(gpu, True)
 
         print('gpus:', gpus)
-        print(tf.config)
+        print(dir(tf.config))
         #self._load_data(kind='train')
 
         if self.pretrained_model is None:
