9fc242b03e62c2f898a8453e0a862c05e57b3a4b
Tom Kimpson <tomkimpson@gmail.com>
2022-09-10 06:30:56 +1000

    recommence V15 training

--- pipeline/configs/config.py
+++ pipeline/configs/config.py
@@ -102,7 +102,7 @@ CFG = {
         "metrics":                 ["accuracy"],
         "path_to_trained_models":  f'{root}processed_data/trained_models/',
         "model_name":              'V15_2016_2_V2', #This model will also be used for prediction
-        "use_pretrained_model":    False,
+        "use_pretrained_model":    True,
         "overwrite":               True,
         "epoch_save_freq":         10,
         "early_stopping_patience": 20,
